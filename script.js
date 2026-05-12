d28ed3d05c417394f7c93c95a38bd0a6ea611bd8
Mariela Polin Flores <marielapolinflores@gmail.com>
2026-05-12 00:06:02 -0500

    Por fin subiendo el script

---
+++ script.js
@@ -0,0 +1,52 @@
+function crearTextoCircular(id, texto, radio){
+
+    const contenedor = document.getElementById(id);
+
+    const letras = texto.split("");
+
+    letras.forEach((letra, i)=>{
+
+        const span = document.createElement("span");
+
+        span.innerText = letra;
+
+        const angulo = (360 / letras.length) * i;
+
+        span.style.transform =
+        `
+        rotate(${angulo}deg)
+        translate(${radio}px)
+        rotate(90deg)
+        `;
+
+        contenedor.appendChild(span);
+
+    });
+
+}
+
+/* TEXTOS */
+
+crearTextoCircular(
+"texto1",
+" ♥ TE AMO MAMÁ ♥ JUNTAS POR SIEMPRE♥ ",
+330
+);
+
+crearTextoCircular(
+"texto2",
+" ♥ ERES MI GUÍA ♥ GRACIAS POR TODO ♥ ",
+450
+);
+
+crearTextoCircular(
+"texto3",
+" ♥ FELIZ DÍA MAMÁ ♥ TE QUIERO MUCHO ♥ ",
+580
+);
+
+crearTextoCircular(
+"texto4",
+" ♥ SIEMPRE A TU LADO ♥ ERES LA MEJOR ♥ ",
+700
+);
